4ef29ad2e8605d9238dd6c5e6e6871e49c2fbed6
Tomer Sagi <ts.tomersagi@gmail.com>
2023-10-09 14:30:47 +0200

    bugfixing types code

--- datasets/templatetags/dataset_extras.py
+++ datasets/templatetags/dataset_extras.py
@@ -160,6 +160,7 @@ def url_it(val):
     return val.replace(reg.group(1), '<a href="' + reg.group(
         1) + '" target="_blank">link <i class="fa fa-external-link"></i> </>') if reg else val
 
+
 @register.filter(name='get_item')
 def get_item(dictionary, key):
     return dictionary.get(key)
