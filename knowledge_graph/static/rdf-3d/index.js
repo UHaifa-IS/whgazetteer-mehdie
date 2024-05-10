91e519680cb06c489a9012ca1f55347397caad84
Tomer Sagi <ts.tomersagi@gmail.com>
2024-05-10 14:22:52 +0200

    Working on #182. cleanup and add reference to D3RDF

--- knowledge_graph/static/rdf-3d/index.js
+++
@@ -1,5 +0,0 @@
-import App from "./App.js"
-import Visualizer from "./Visualizer.js"
-
-customElements.define(App.ELEMENT_NAME, App, {extends: "main"})
-customElements.define(Visualizer.ELEMENT_NAME, Visualizer, {extends: "canvas"})
