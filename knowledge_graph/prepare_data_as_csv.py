8836951a03a67c44ebfe83158bb46f250a46048c
Tomer Sagi <ts.tomersagi@gmail.com>
2024-05-13 13:04:40 +0200

    new ttl map 2

--- knowledge_graph/prepare_data_as_csv.py
+++ knowledge_graph/prepare_data_as_csv.py
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     # Dump the tables
     dump_table('datasets', ['id', 'label', 'title', 'description', 'webpage', 'uri_base', 'datatype'])
     logging.info('Dumped datasets table')
-    dump_table('place', ['id', 'title', 'src_id'])
+    dump_table('places', ['id', 'title', 'src_id'])
     logging.info('Dumped place table')
